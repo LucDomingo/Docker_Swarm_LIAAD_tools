02194bf2ef9742ef263871c66ae722ef15bc9abe
LucDomingo <47193801+LucDomingo@users.noreply.github.com>
2019-08-09 14:52:21 +0200

    Create update_file.sh

---
+++ update_file.sh
@@ -0,0 +1,3 @@
+docker-machine scp nginx.conf swarm-manager:/home/docker-user/nginx.conf;
+docker-machine scp nginx.conf swarm-worker-1:/home/docker-user/nginx.conf;
+docker-machine scp nginx.conf swarm-worker-2:/home/docker-user/nginx.conf;
